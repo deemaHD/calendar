6504fee9506b9d5436c1e184d5948878c2ab8e36
Dmitriy Selezen <dima.selezen@rambler.ru>
2015-08-18 21:49:14 +0300

    First route done

--- app/Controller.js
+++ app/Controller.js
@@ -13,11 +13,33 @@
             startDate.setMonth(startDate.getMonth() + 1);
             $('#main').append(monthView.render(startDate).el);
             
+            setUpMediator();
             findFirstWeek(); // add class 'start' to every first week
+        }
+        
+        function setUpMediator () {
+            App.mediator.subscribe('ShowCalendar', showCalendar, {}, this);
+            App.mediator.subscribe('ShowWeek', showWeek, {}, this);
+        }
+        
+        function showCalendar () {
+            $('.calendar').removeClass('hiden');
+            $('.preview').remove();
+        }
+        
+        function showWeek (previewWeek) {
+            hideAll();
+            var weekPreView = new WeekPreView({model: previewWeek});
+            
+            $('#main').append(weekPreView.render(previewWeek.id).el);
         }
             
         function findFirstWeek () {
             monthView.findFirstWeek();
+        }
+        
+        function hideAll () {
+            $('#main').children().addClass('hiden');
         }
     }
 })(App);
--- app/Router.js
+++ app/Router.js
@@ -1,15 +1,32 @@
 (function (This) {
     This.Router = Backbone.Router.extend({
         routes: {
-            '': 'mainView'
+            '': 'mainView',
+            'calendar': 'mainView',
+            'week/:id': 'showWeek'
         },
 
         initialize: function () {
-            this.controller = new App.Controller();
+            this.controller = new This.Controller();
+            
+            This.mediator.subscribe('ShowWeek', this.navigateShowWeek, {}, this);
+            This.mediator.subscribe('ShowCalendar', this.navigateShowCalendar, {}, this);
         },
         
         mainView: function () {
-            this.navigate('');
+            This.mediator.publish('ShowCalendar');
+        },
+        
+        navigateShowWeek: function (week) {
+            this.navigate('week/' + week.id);
+        },
+        
+        navigateShowCalendar: function () {
+            this.navigate('calendar');    
+        },
+        
+        showWeek: function (id) {
+            This.mediator.publish('ShowWeek', id);
         }
     });
 })(App);
